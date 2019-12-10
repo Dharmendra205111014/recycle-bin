3755fd3ee0438ad72acfdddb57ad7474caffa0af
Dharmendra <dharmendra.patel26@gmail.com>
2019-12-10 23:55:24 +0530

    initial commit :tada:

---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    This is Home
+  </div>
+</template>
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component({})
+export default class Home extends Vue {
+
+}
+</script>
